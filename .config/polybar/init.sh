f5df2211f90be9db9a92afca410655b105cdd474
smcmanis <simon.mcmanis@gmail.com>
2021-07-13 18:25:23 +1000

    Configure polybar for dual monitors

--- .config/polybar/init.sh
+++ .config/polybar/init.sh
@@ -5,10 +5,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-# while pgrep -x polybar >/dev/null; do sleep 1; done
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar main -c $HOME/.config/polybar/config.ini &
-# polybar main -c $(dirname $0)/config.ini &
+polybar eDP1 -c $HOME/.config/polybar/config.ini &
+polybar HDMI1 -c $(dirname $0)/config.ini &
 
