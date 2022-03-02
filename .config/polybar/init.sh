8e7ce4bb14f6b827f33e2b0ac4a4973c02cdb074
smcmanis <simon.mcmanis@gmail.com>
2022-03-02 18:21:23 +1100

    Launch polybar instance for each connected monitor

--- .config/polybar/init.sh
+++ .config/polybar/init.sh
@@ -1,18 +1,13 @@
 
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
-killall -q polybar
-
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-HDMI_STATUS="$(xrandr --query | grep HDMI1 | cut -d ' ' -f 2)"
-
-if [ $HDMI_STATUS == "connected" ]; then
-  polybar HDMI1 -c $(dirname $0)/config.ini &
-else
-  polybar eDP1 -c $HOME/.config/polybar/config.ini &
-fi
-
+for m in "eDP1" "HDMI1" "DP1" "DP2"; do
+  status="$(xrandr | grep -w connected | grep -w $m | cut -d ' ' -f 2)"
+  if [[ $status == "connected" ]]; then
+    polybar $m -c /home/simon/.config/polybar/config.ini &
+  fi
+done
